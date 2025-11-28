556ecdc29e78096a605841251ab4a70c99cf2c4b
chuwi <srpc9682@protonmail.com>
2025-11-28 10:41:01 +0100

    Add CalatolgBookRepository.java

---
+++ src/main/java/dedede/repository/CatalogBookRepository.java
@@ -0,0 +1,52 @@
+package dedede.repository;
+
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Optional;
+
+public class CatalogBookRepository implements IRepositorioExtend{
+    @Override
+    public Optional findByIdOptional(Object o) {
+        return Optional.empty();
+    }
+
+    @Override
+    public List findAllList() throws SQLException {
+        return List.of();
+    }
+
+    @Override
+    public long count() {
+        return 0;
+    }
+
+    @Override
+    public void deleteById(Object o) {
+
+    }
+
+    @Override
+    public void deleteAll() {
+
+    }
+
+    @Override
+    public boolean existsById(Object o) {
+        return false;
+    }
+
+    @Override
+    public Object findById(Object o) throws SQLException {
+        return null;
+    }
+
+    @Override
+    public Iterable findAll() throws SQLException {
+        return null;
+    }
+
+    @Override
+    public Object save(Object entity) throws SQLException {
+        return null;
+    }
+}
