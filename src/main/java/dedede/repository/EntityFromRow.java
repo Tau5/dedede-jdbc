d7cb35ad72a0609a5da7965f9ba2782f9a1380c8
chuwi <srpc9682@protonmail.com>
2025-12-02 08:59:44 +0100

    Rename interface

--- src/main/java/dedede/repository/EntityFromRow.java
+++ src/main/java/dedede/repository/EntityFromRow.java
@@ -3,6 +3,6 @@ package dedede.repository;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-public interface FromRow<T> {
+public interface EntityFromRow<T> {
     T fromRow(ResultSet res) throws SQLException;
 }
