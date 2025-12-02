28d776ac04d121bd78ec95af93070b805f9ba0f8
chuwi <srpc9682@protonmail.com>
2025-12-02 09:27:07 +0100

    Refactor code

--- src/main/java/dedede/repository/UserRepository.java
+++ src/main/java/dedede/repository/UserRepository.java
@@ -1,125 +1,133 @@
 package dedede.repository;
 
 import dedede.domain.User;
-import dedede.infrastructure.CSVManager;
-import dedede.infrastructure.CSVRow;
 
+import javax.xml.transform.Result;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-public class UserRepository implements IRepositorioExtend<User, Long> {
+public class UserRepository implements IRepositorioExtend<User, Long>, ToStatement<User>  {
 
-    private CSVManager table;
+    private Connection connection;
 
-    public UserRepository(File file) throws IOException {
-        if (!file.exists()) {
-            file.getParentFile().mkdirs();
-            var writer = new BufferedWriter(new FileWriter(file));
-            writer.write("id,name,surnames");
-            writer.flush();
-            writer.close();
-        }
-        this.table = new CSVManager(file);
+    public UserRepository(Connection connection) throws IOException {
+        this.connection = connection;
     }
 
-    private User userFromRow(CSVRow row) throws InvalidRowException {
+    private User userFromRow(ResultSet rs) throws SQLException {
         return new User(
-                row.getLong(0).orElseThrow(() -> new InvalidRowException("Couldn't parsing column 0 to id")),
-                row.getString(1).orElseThrow(() -> new InvalidRowException("Couldn't parsing column 1 to name")),
-                row.getString(2).orElseThrow(() -> new InvalidRowException("Couldn't parsing column 2 to surname"))
+                rs.getLong(1),
+                rs.getString(2),
+                rs.getString(3)
         );
-    }
-
-    private CSVRow userToRow(User user) {
-        CSVRow row = new CSVRow(3);
-        row.setLong(0, user.getID());
-        row.setString(1, user.getName());
-        row.setString(2, user.getSurname());
-        return row;
     }
 
     @Override
     public long count() {
-        return (long) table.listAll().size();
-    }
 
+        long count = 0;
+
+        var query = "SELECT count(*) FROM User;";
+        ResultSet rs;
+        try {
+            var st = connection.createStatement();
+            rs = st.executeQuery(query);
+            count = (long) rs.getLong(1);
+            st.close();
+            rs.close();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return count;
+    }
     @Override
     public void deleteById(Long id) {
-        table.deleteRow(id.toString(), 0);
+        var query = "DELETE FROM User WHERE id = ?;";
+        try {
+            var st = connection.prepareStatement(query);
+            st.setLong(1, id);
+            st.executeQuery(query);
+            st.close();
+        } catch (SQLException e) {
+            throw new RuntimeException("We can't delete usesr with " + id + " because:" + e);
+        }
     }
 
     @Override
     public void deleteAll() {
-        table.emptyTable();
+        var query = "DELETE FROM User;";
+
+        try {
+            var st = connection.createStatement();
+            st.executeQuery(query);
+            st.close();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
     public boolean existsById(Long ID) {
+        var query = "SELECT * FROM usuario WHERE id = ?;";
         boolean exist = false;
-        var rows = table.listAll();
+        ResultSet rs;
+        try {
+            var st = connection.prepareStatement(query);
+            st.setLong(1, ID);
+            rs = st.executeQuery(query);
+            if (rs.getBoolean(String.valueOf(ID))) {
+                exist = true;
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
 
-       exist = rows.stream().filter(f -> f.getLong(0).get().equals(ID)).findFirst().isPresent();
-
-       return exist;
+        return exist;
     }
 
     @Override
-    public User findById(Long id) {
-        var rows = table.listAll();
-        var maybeUser = rows.stream().filter(
-                f -> f.getLong(0).filter(
-                        l -> l.equals(id)).isPresent()
-        ).findFirst();
-        var user = maybeUser.map(row -> {
-            try {
-                return userFromRow(row);
-            } catch (InvalidRowException e) {
-                throw new RuntimeException(e);
-            }
-        }).get();
+    public User findById(Long id) throws SQLException {
+        var query = "SELECT * FROM usuario WHERE id = ?";
+        ResultSet rs;
+        User user;
+        var st = connection.prepareStatement(query);
+        st.setLong(1, id);
+        rs = st.executeQuery(query);
+        user = userFromRow(rs);
         return user;
     }
 
     @Override
-    public Iterable<User> findAll() {
-        return table.listAll().stream().map(row -> {
-            try {
-                return userFromRow(row);
-            } catch (InvalidRowException e) {
-                throw new RuntimeException(e);
-            }
-        }).toList();
+    public Iterable<User> findAll() throws SQLException {
+        List<User> users = new ArrayList<>();
+        var query = "SELECT * FROM User;";
+        var st = connection.createStatement();
+        ResultSet rs = st.executeQuery(query);
+        while (rs.next()) {
+            users.add(userFromRow(rs));
+        }
+        return users;
     }
 
     @Override
-    public <S extends User> S save(S entity) {
-        long newId = 0;
-
-        if (findAllList().isEmpty()) {
-            newId = 0;
+    public <S extends User> S save(S user) throws SQLException {
+        if (existsById(user.getID())) {
+            var stUpdate = toUpdate(user);
+            stUpdate.executeUpdate();
+            stUpdate.close();
+            return user;
         } else {
-            newId = findAllList().getLast().getID() + 1;
+            var stInsert = toInsert(user);
+            stInsert.executeQuery();
+            stInsert.close();
+            return user;
         }
-
-        try {
-            if(existsById(entity.getID())) {
-                CSVRow row = userToRow(entity);
-                table.updateRow(CSVManager.convertToRaw(entity.getID()), 0, row);
-            } else {
-                entity.setID(newId);
-                CSVRow row = userToRow(entity);
-                table.insertRow(row);
-            }
-
-            table.saveFile();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-        return entity;
     }
 
     @Override
@@ -128,7 +136,32 @@ public class UserRepository implements IRepositorioExtend<User, Long> {
     }
 
     @Override
-    public List<User> findAllList() {
-        return (List<User>) this.findAll();
+    public List<User> findAllList() throws SQLException {
+        List<User> users = new ArrayList<>();
+        var query = "SELECT * FROM User;";
+        var st = connection.createStatement();
+        ResultSet rs = st.executeQuery(query);
+        while (rs.next()) {
+            users.add(userFromRow(rs));
+        }
+        return users;
+    }
+
+    @Override
+    public PreparedStatement toUpdate(User user) throws SQLException {
+        PreparedStatement ps = connection.prepareStatement("UPDATE User SET name = ?, surname = ? WHERE id = ?;");
+        ps.setString(1, user.getName());
+        ps.setString(2, user.getSurname());
+        ps.setLong(3, user.getID());
+
+        return ps;
+    }
+
+    @Override
+    public PreparedStatement toInsert(User user) throws SQLException {
+        PreparedStatement ps = connection.prepareStatement("INSERT INTO User (name, surname) VALUES (?, ?) RETURNING *;");
+        ps.setString(1, user.getName());
+        ps.setString(2, user.getSurname());
+        return ps;
     }
 }
