284b4f5eb5b9ff583e0f1a965559c41d84a10cc2
Tau <tau0@tutanota.com>
2025-12-02 08:59:27 +0100

    add docs for FromRow and ToStatement

--- src/main/java/dedede/repository/FromRow.java
+++ src/main/java/dedede/repository/FromRow.java
@@ -4,5 +4,13 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public interface FromRow<T> {
+    /**
+     * Given a ResultSet row from a SELECT statement which returns ALL rows
+     * of the table, returns a new Entity
+     *
+     * @param res Result set from a SELECT * from Entity
+     * @return T Entity
+     * @throws SQLException
+     */
     T fromRow(ResultSet res) throws SQLException;
 }
