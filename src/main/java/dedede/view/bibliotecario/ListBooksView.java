727b6afcd42f9b5d446ba5a6e167739bda1fe5e6
chuwi <srpc9682@protonmail.com>
2026-01-30 10:47:35 +0100

    Fix Viw ListBooksView.java

--- src/main/java/dedede/view/bibliotecario/ListBooksView.java
+++ src/main/java/dedede/view/bibliotecario/ListBooksView.java
@@ -4,13 +4,19 @@ import dedede.view.Model;
 import dedede.view.View;
 import dedede.view.ViewManager;
 
+import java.sql.SQLException;
+
 public class ListBooksView implements View {
 
     @Override
     public void run(Model model, ViewManager viewManager) {
 
         var books = model.books;
-        books.findAllList().forEach(System.out::println);
+        try {
+            books.findAllList().forEach(System.out::println);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
         viewManager.switchView(new ViewBibliotecario());
     }
 }
