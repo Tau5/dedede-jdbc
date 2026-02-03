7564b1726904d1957787a68ad7188246f9620275
Pablo Alonso <tau0@tutanota.com>
2026-02-03 19:56:29 +0100

    this one too

--- src/main/java/dedede/services/CommodateService.java
+++ src/main/java/dedede/services/CommodateService.java
@@ -2,9 +2,13 @@ package dedede.services;
 
 import dedede.domain.Book;
 import dedede.domain.Commodate;
+import dedede.domain.User;
 import dedede.repository.CommodateRepository;
 
 import java.sql.SQLException;
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.time.temporal.TemporalAmount;
 import java.util.List;
 
 public class CommodateService {
@@ -20,6 +24,12 @@ public class CommodateService {
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    Commodate registerCommodate(Book book, User user) throws SQLException {
+        return commodateRepository.save(
+                new Commodate(-1, Instant.now(), Instant.now().plus(15, ChronoUnit.DAYS), user.getID(), book.getID())
+        );
     }
 
 }
