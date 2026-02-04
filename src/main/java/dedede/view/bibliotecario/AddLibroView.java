56284b43b96f1369e2834a2990101cb98440e2ca
Tau <tau0@tutanota.com>
2026-02-04 12:34:14 +0100

    Adapt views to work with new entity model

--- src/main/java/dedede/view/bibliotecario/AddLibroView.java
+++ src/main/java/dedede/view/bibliotecario/AddLibroView.java
@@ -1,24 +1,35 @@
 package dedede.view.bibliotecario;
 
 import dedede.domain.Book;
+import dedede.domain.CatalogBook;
+import dedede.services.BookService;
+import dedede.services.CatalogService;
 import dedede.view.MenuHelper;
 import dedede.view.Model;
 import dedede.view.View;
 import dedede.view.ViewManager;
 
+import java.sql.SQLException;
+
 public class AddLibroView implements View {
     @Override
     public void run(Model model, ViewManager viewManager) {
+        CatalogService service = new CatalogService(model);
         System.out.print("Título: ");
         MenuHelper.sc.nextLine();
         String title = MenuHelper.sc.nextLine();
         MenuHelper.sc.reset();
         System.out.print("Autor: ");
         String author = MenuHelper.sc.nextLine();
+        System.out.println("ISBN: ");
+        String isbn = MenuHelper.sc.nextLine();
+        int stock = MenuHelper.getNumber("Cantidad: ");
 
-        Book book = new Book(title, author);
-
-        model.books.save(book);
+        try {
+            service.createCatalogBookWithStock(new CatalogBook(isbn, author, title), stock);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
 
         System.out.println("Libro creado satisfactoriamente!");
 
--- src/main/java/dedede/view/bibliotecario/DeleteLibroView.java
+++ src/main/java/dedede/view/bibliotecario/DeleteLibroView.java
@@ -1,28 +1,48 @@
 package dedede.view.bibliotecario;
 
+import dedede.domain.CatalogBook;
 import dedede.view.MenuHelper;
 import dedede.view.Model;
 import dedede.view.View;
 import dedede.view.ViewManager;
 
+import java.sql.SQLException;
+import java.util.Optional;
+
 public class DeleteLibroView implements View {
     @Override
     public void run(Model model, ViewManager viewManager) {
-        model.books.findAll().forEach(book -> {
-            System.out.println(book);
-        });
+        try {
+            model.catalog.findAll().forEach(book -> {
+                System.out.println(book);
+            });
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
 
-        int id = MenuHelper.getNumber("ID a eliminar");
+        System.out.print("ISBN a eliminar: ");
+        String id = MenuHelper.sc.nextLine();
 
-        var maybeBook = model.books.findByIdOptional((long) id);
-        maybeBook.ifPresentOrElse(
-            book -> {
-                model.books.deleteById(book.getID());
-            },
-            () -> {
-                System.out.println("Error: No existe un libro con ese ID");
-            }
-        );
+        Optional<CatalogBook> maybeBook = null;
+        try {
+            maybeBook = model.catalog.findByIdOptional(id);
+
+            maybeBook.ifPresentOrElse(
+                    book -> {
+                        try {
+                            model.catalog.deleteById(book.getISBN());
+                        } catch (SQLException e) {
+                            throw new RuntimeException(e);
+                        }
+                    },
+                    () -> {
+                        System.out.println("Error: No existe un libro con ese ID");
+                    }
+            );
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
 
         viewManager.switchView(new ViewBibliotecario());
     }
